1ebc12283005ea87bc407361b06af8f37d3bf253
jnickg <me@jnickg.net>
2024-04-09 15:07:59 -0700

    add parameterized get

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, hello])
 }
 
