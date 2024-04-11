c10037e49fc00fac4e7103b13170f2f7e773ed11
jnickg <me@jnickg.net>
2024-04-11 13:12:23 -0700

    Rename to CircularBuffer

--- src/circular_buffer.rs
+++ src/circular_buffer.rs
@@ -5,7 +5,7 @@ impl<T> BufferElement for T where T: Clone + Copy + Default {}
 
 /// A Circular Buffer whose internal storage is known at compile time
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
-pub struct MyCicularBuffer<T: BufferElement, const N: usize> {
+pub struct CircularBuffer<T: BufferElement, const N: usize> {
     /// The actual data storage
     data: [T; N],
 
@@ -22,7 +22,7 @@ pub struct MyCicularBuffer<T: BufferElement, const N: usize> {
     overwrites: bool,
 }
 
-impl<T: BufferElement, const N: usize> Default for MyCicularBuffer<T, N> {
+impl<T: BufferElement, const N: usize> Default for CircularBuffer<T, N> {
     fn default() -> Self {
         Self {
             data: [T::default(); N],
@@ -34,7 +34,7 @@ impl<T: BufferElement, const N: usize> Default for MyCicularBuffer<T, N> {
     }
 }
 
-impl<T: BufferElement, const N: usize> MyCicularBuffer<T, N> {
+impl<T: BufferElement, const N: usize> CircularBuffer<T, N> {
     pub fn new(data: [T; N]) -> Self {
         Self {
             data,
@@ -114,7 +114,7 @@ impl<T: BufferElement, const N: usize> MyCicularBuffer<T, N> {
     }
 }
 
-impl<T: BufferElement, const N: usize> Index<usize> for MyCicularBuffer<T, N> {
+impl<T: BufferElement, const N: usize> Index<usize> for CircularBuffer<T, N> {
     type Output = T;
 
     fn index(&self, i: usize) -> &Self::Output {
@@ -123,7 +123,7 @@ impl<T: BufferElement, const N: usize> Index<usize> for MyCicularBuffer<T, N> {
     }
 }
 
-impl<T: BufferElement, const N: usize> IndexMut<usize> for MyCicularBuffer<T, N> {
+impl<T: BufferElement, const N: usize> IndexMut<usize> for CircularBuffer<T, N> {
     fn index_mut(&mut self, i: usize) -> &mut Self::Output {
         let actual_index = i % self.size;
         &mut self.data[actual_index]
@@ -136,7 +136,7 @@ mod tests {
 
     #[test]
     fn circular_buffer_appends_until_full_when_non_overwriting() {
-        let mut buffer = MyCicularBuffer::<u32, 3>::new_empty();
+        let mut buffer = CircularBuffer::<u32, 3>::new_empty();
         assert_eq!(buffer.append(1), Ok(1));
         assert_eq!(buffer.append(2), Ok(2));
         assert_eq!(buffer.append(3), Ok(3));
@@ -146,7 +146,7 @@ mod tests {
 
     #[test]
     fn circular_buffer_appends_forever_when_overwriting() {
-        let mut buffer = MyCicularBuffer::<u32, 3>::new_empty_overwriting();
+        let mut buffer = CircularBuffer::<u32, 3>::new_empty_overwriting();
         assert_eq!(buffer.append(1), Ok(1));
         assert_eq!(buffer.append(2), Ok(2));
         assert_eq!(buffer.append(3), Ok(3));
@@ -158,7 +158,7 @@ mod tests {
 
     #[test]
     fn circular_buffer_indexes_repeatedly() {
-        let buffer = MyCicularBuffer::new([1, 2, 3]);
+        let buffer = CircularBuffer::new([1, 2, 3]);
         assert_eq!(buffer[0], 1);
         assert_eq!(buffer[1], 2);
         assert_eq!(buffer[2], 3);
@@ -169,7 +169,7 @@ mod tests {
 
     #[test]
     fn circular_buffer_mutates_repeatedly() {
-        let mut buffer = MyCicularBuffer::new([1, 2, 3]);
+        let mut buffer = CircularBuffer::new([1, 2, 3]);
         buffer[0] = 4;
         buffer[1] = 5;
         buffer[2] = 6;
@@ -184,7 +184,7 @@ mod tests {
 
     #[test]
     fn circular_buffer_pop_repeatedly() {
-        let mut buffer = MyCicularBuffer::new([1, 2, 3]);
+        let mut buffer = CircularBuffer::new([1, 2, 3]);
         assert_eq!(buffer.pop(), Ok(3));
         assert_eq!(buffer.pop(), Ok(2));
         assert_eq!(buffer.pop(), Ok(1));
