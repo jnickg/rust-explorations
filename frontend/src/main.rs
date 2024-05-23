16d35c9eb3236088e760c845eaa6ec07c3bccf4d
jnickg <me@jnickg.net>
2024-05-23 14:47:21 -0700

    Start scaffolding frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,83 +1,167 @@
-use gloo_net::http::Request;
-use yew::prelude::*;
-use yew_router::prelude::*;
-use wasm_bindgen_futures::spawn_local;
+extern crate base64;
+use std::collections::HashMap;
 
-#[derive(Clone, Routable, PartialEq)]
-enum Route {
-    #[at("/")]
-    Home,
-    #[at("/hello-server")]
-    HelloServer
+use base64::engine::general_purpose::STANDARD;
+use base64::Engine;
+use gloo::file::callbacks::FileReader;
+use gloo::file::File;
+use web_sys::{DragEvent, Event, FileList, HtmlInputElement};
+use yew::{html, Callback, Component, Context, Html, TargetCast};
+use image::DynamicImage;
+
+struct FileDetails {
+    name: String,
+    file_type: String,
+    data: Vec<u8>,
 }
 
-fn switch(routes: Route) -> Html {
-    match routes {
-        Route::Home => html! { <h1>{ "Hello Frontend" }</h1> },
-        Route::HelloServer => html! { <HelloServer /> }
+pub enum Msg {
+    Loaded(String, String, Vec<u8>),
+    Files(Vec<File>),
+    Pyramid(String),
+    PyramidTiles(String),
+}
+
+pub struct App {
+    readers: HashMap<String, FileReader>,
+    files: Vec<FileDetails>,
+    pyramid_ids: HashMap<String, Option<Vec<Vec<DynamicImage>>>>,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            readers: HashMap::default(),
+            files: Vec::default(),
+            pyramid_ids: HashMap::default(),
+        }
     }
-}
 
-#[function_component(HelloServer)]
-fn hello_server() -> Html {
-    let data = use_state(|| None);
-
-    // Request `/api/hello` once
-    {
-        let data = data.clone();
-        use_effect(move || {
-            if data.is_none() {
-                spawn_local(async move {
-                    let resp = Request::get("/api/v1/hello").send().await.unwrap();
-                    let result = {
-                        if !resp.ok() {
-                            Err(format!(
-                                "Error fetching data {} ({})",
-                                resp.status(),
-                                resp.status_text()
-                            ))
-                        } else {
-                            resp.text().await.map_err(|err| err.to_string())
-                        }
-                    };
-                    data.set(Some(result));
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::PyramidTiles(pyramid_id) => {
+                let tile_hierarchy: Vec<Vec<DynamicImage>> = Vec::default();
+                // TODO fetch each tile, fetch them and put them into the tile hierarchy
+                
+                self.pyramid_ids.insert(pyramid_id.clone(), Some(tile_hierarchy));
+                true
+            }
+            Msg::Pyramid(pyramid_id) => {
+                self.pyramid_ids.insert(pyramid_id.clone(), None);
+                // TODO set up some background async poll for the pyramid's "tiles" state, and
+                // when they are available, send the appropriate message so we know to render them
+                // on screen.
+                true
+            }
+            Msg::Loaded(file_name, file_type, data) => {
+                self.files.push(FileDetails {
+                    data,
+                    file_type,
+                    name: file_name.clone(),
                 });
+                self.readers.remove(&file_name);
+                // TODO POST to backend the image to <hostname>:<port>/api/v1/pyramid with the
+                // image data and mime type.  Use a callback that handles the return of that
+                // request, parses the JSON for the pyramid ID, and sends an appropriate message
+                // like how we send link.send_message below
+                true
             }
+            Msg::Files(files) => {
+                for file in files.into_iter() {
+                    let file_name = file.name();
+                    let file_type = file.raw_mime_type();
 
-            || {}
-        });
+                    let task = {
+                        let link = ctx.link().clone();
+                        let file_name = file_name.clone();
+
+                        gloo::file::callbacks::read_as_bytes(&file, move |res| {
+                            link.send_message(Msg::Loaded(
+                                file_name,
+                                file_type,
+                                res.expect("failed to read file"),
+                            ))
+                        })
+                    };
+                    self.readers.insert(file_name, task);
+                }
+                true
+            }
+        }
     }
 
-    match data.as_ref() {
-        None => {
-            html! {
-                <div>{"No server response"}</div>
-            }
-        }
-        Some(Ok(data)) => {
-            html! {
-                <div>{"Got server response: "}{data}</div>
-            }
-        }
-        Some(Err(err)) => {
-            html! {
-                <div>{"Error requesting data from server: "}{err}</div>
-            }
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div id="wrapper">
+                <p id="title">{ "Upload Your Files To The Cloud" }</p>
+                <label for="file-upload">
+                    <div
+                        id="drop-container"
+                        ondrop={ctx.link().callback(|event: DragEvent| {
+                            event.prevent_default();
+                            let files = event.data_transfer().unwrap().files();
+                            Self::upload_files(files)
+                        })}
+                        ondragover={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                        ondragenter={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                    >
+                        <i class="fa fa-cloud-upload"></i>
+                        <p>{"Drop your images here or click to select"}</p>
+                    </div>
+                </label>
+                <input
+                    id="file-upload"
+                    type="file"
+                    accept="image/*"
+                    onchange={ctx.link().callback(move |e: Event| {
+                        let input: HtmlInputElement = e.target_unchecked_into();
+                        Self::upload_files(input.files())
+                    })}
+                />
+                <div id="preview-area">
+                    { for self.files.iter().map(Self::view_file) }
+                </div>
+            </div>
         }
     }
 }
 
+impl App {
+    fn view_file(file: &FileDetails) -> Html {
+        html! {
+            <div class="preview-tile">
+                <p class="preview-name">{ format!("{}", file.name) }</p>
+                <div class="preview-media">
+                    if file.file_type.contains("image") {
+                        <img src={format!("data:{};base64,{}", file.file_type, STANDARD.encode(&file.data))} />
+                    }
+                </div>
+            </div>
+        }
+    }
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <BrowserRouter>
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
+    fn upload_files(files: Option<FileList>) -> Msg {
+        let mut result = Vec::new();
+
+        if let Some(files) = files {
+            let files = js_sys::try_iter(&files)
+                .unwrap()
+                .unwrap()
+                .map(|v| web_sys::File::from(v.unwrap()))
+                .map(File::from);
+            result.extend(files);
+        }
+        Msg::Files(result)
     }
 }
+
 fn main() {
-    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
-    console_error_panic_hook::set_once();
     yew::Renderer::<App>::new().render();
 }
